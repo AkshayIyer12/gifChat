7e84fc16350e75ac84a4e9d0b6eed754d33b007c
Kartik <www.kkartik001@gmail.com>
2018-02-25 00:35:08 +0530

    Add logic for webcam and image reader

--- public/js/app.js
+++ public/js/app.js
@@ -17,4 +17,37 @@
       ul.innerHTML += li
     }
   })
+  document.getElementById('read_img').addEventListener('change', function readURL (input) {
+    let files = input.explicitOriginalTarget.files
+    if (files && files[0]) {
+      let reader = new FileReader()
+      reader.onload = function (e) {
+        [['src', e.target.result], ['width', 200], ['height', 350]].map(([a, b]) => setAttr(a, b))
+      }
+      reader.readAsDataURL(files[0])
+    }
+  })
+  function setAttr (key, value) {
+    document.getElementById('blah').setAttribute(key, value)
+  }
+  let video = document.querySelector('#videoElement')
+  let canvas = document.getElementById('canvas')
+  let data = canvas.getContext('2d')
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia
+
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({video: true}, handleVideo, videoError)
+  }
+
+  function handleVideo (stream) {
+    video.src = window.URL.createObjectURL(stream)
+  }
+
+  function videoError (e) {
+    console.error(e)
+  }
+
+  document.getElementById('capture').addEventListener('click', function () {
+    data.drawImage(video, 0, 0, 400, 300)
+  })
 })()
